b5bdefc30e62b5005653edf1acc58faf97a91026
Roberto Carriero <roberto_carriero1@yahoo.com.arm>
2023-11-29 17:42:02 -0300

    [cfg] - base structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto aplicación para ver películas consumiendo API TMDB
+
+Link como instalar entorno para testear
+https://www.robinwieruch.de/vitest-react-testing-library/
---
+++ src/App.spec.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+import {describe,expect, it} from "vitest";
+
+
+
+describe("App", () => {
+    it("should render component", () => {
+      const component = render(<App />);
+      expect(component).toBeDefined();
+    });
+  });
+  
